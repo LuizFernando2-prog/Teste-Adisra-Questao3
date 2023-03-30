c69062d77118d6bb50edbeba011ce9b4eb72c6e2
LuizFernando2-prog <69861259+LuizFernando2-prog@users.noreply.github.com>
2023-03-30 16:55:04 -0300

    Add files via upload

---
+++ Teste-Adisra-questao3/Program.cs
@@ -0,0 +1,15 @@
+﻿int ff(int n)
+{
+    if (n == 1) return 1;
+    if (n % 2 == 0) return ff(n / 2);
+    return ff((n - 1) / 2) + ff((n + 1) / 2);
+}
+void main()
+{
+    printf("%d", ff(7));
+    return;
+}
+
+/*
+ Esse codigo nao compila, ja que esta escrito em C. Porem fiz um teste em Python e o retorno foi 3.
+ */
